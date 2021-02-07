3fad33d92342a6dff11e3c8251d55c3dd8b085c9
PetrikAnna-Mariia <78664437+PetrikAnna-Mariia@users.noreply.github.com>
2021-02-07 16:41:26 +0200

    refactored logic

--- timeTable.js
+++ timeTable.js
@@ -11,10 +11,7 @@ let addNewParty = document.getElementById('addNewParty');
 let inputNewParty = document.getElementById('newParty');
 let selectPartys = ["reset", "Чебурашка", "Крокодил Гена", "Шапокляк", "Крыса Лариса"];
 let littleButton = document.getElementById("littleButton");
-let eventMap = new Map();
-let startDate = getCurrentMonday();
-let hrs = [];
-let tds = [];
+let eventMap = new Map;
 create.addEventListener("click", checkForm);
 cancel.addEventListener('click', cleanForm);
 addNewParty.addEventListener('click', addNewMember);
@@ -25,32 +22,23 @@ function removeEvent(event) {
     let flag = confirm("You relly want to delete events?");
     if (flag == false) return;
     eventMap.delete(cell);
-    cell.style.backgroundColor = "rgb(255, 248, 220)";
+    cell.setAttribute("class","cornsilk")
     cell.innerHTML = " ";
     cell.removeEventListener("click", removeEvent);
 }
 
 function filterName(event) {
     let value = event.currentTarget.value;
-    let td = Array.from(document.body.getElementsByTagName('td'))
-    td.forEach(item => {
-        if (!(getComputedStyle(item).backgroundColor == "rgb(255, 248, 220)")) {
-            item.style.backgroundColor = "#00FA9A";
-        };
-    });
-    td.forEach(item => {
-        if (getComputedStyle(item).backgroundColor == "rgb(0, 250, 154)") {
-            if (item.innerHTML.indexOf(value) != -1) {
-                item.style.backgroundColor = "#FFC0CB";
-            }
-
-        };
+    eventMap.forEach((item, cell)=>{
+        cell.setAttribute("class","green");
+        if(item.participants.indexOf(value) != -1){
+            cell.setAttribute("class","pink");
+        }
     });
 };
 
 function addNewMember() {
     if (inputNewParty.value == "") {
-        alert("fdgv");
         return
     }
     createTag(partys, "option", " ", inputNewParty.value);
@@ -62,10 +50,9 @@ function checkForm() {
     let time = arrTime.indexOf(selectedTime.value);
     let day = arrDay.indexOf(selectDay.value);
     let cell = table.rows[time].cells[day];
-    let currentDay = table.rows[0].cells[day];
     let selected = Array.from(partys.selectedOptions).map(option => option.value);
 
-    if (eventMap.has(cell) && eventMap.get(cell).currentDay == currentDay.getAttribute('id')) {
+    if (eventMap.has(cell)) {
         createMessageUpper("This time is booked out");
         return;
     };
@@ -73,26 +60,24 @@ function checkForm() {
         createMessageUpper("Please fill out the form");
         return;
     };
-    createNewEvent(cell, selected, currentDay);
+    createNewEvent(cell, selected);
 };
 
-function createNewEvent(cell, selected, currentDay) {
-
+function createNewEvent(cell, selected) {
+    
     let objForm = {
-        currentDay: currentDay.getAttribute('id'),
         name: inputName.value,
         participants: selected,
-        time: time,
-        day: day
     };
     decorated(cell, objForm);
     eventMap.set(cell, objForm);
 };
 
 function decorated(cell, objForm) {
-    cell.style.backgroundColor = "#00FA9A";
-    cell.innerHTML = objForm.name + "<br>" + objForm.participants.join(' ');
-    let a = createTag(cell, 'button', 'littleButton', "X").addEventListener("click", removeEvent);
+    cell.setAttribute("class","green");
+    cell.innerHTML = objForm.name;
+    let button = createTag(cell, 'button', 'littleButton', "X").addEventListener("click", removeEvent);
+    
 };
 
 function cleanForm() {
@@ -125,77 +110,25 @@ function createContainer() {
     createTag(head, "select", "filterByName").addEventListener('change', filterName);
     createSelect(selectPartys, document.getElementById("filterByName"), "option");
     createTag(head, "button", "", "New event +").addEventListener("click", showForm);
-    createTag(head, "button", "", "< Previous week").addEventListener('click', previousWeek);
-    createTag(head, "button", "", "Next week >").addEventListener('click', nextWeek);
-    createTable(getCurrentMonday());
+    createTable();
 };
 
-function getCurrentMonday() {
-    let currentDate = new Date();
-    let day = currentDate.getDay();
-    currentDate.setDate(currentDate.getDate() - day + 1);
-    return currentDate;
-};
-
-function nextWeek() {
-    startDate = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() + 7);
-    reRenderTable();
-};
-
-function previousWeek() {
-    startDate = new Date(startDate.getFullYear(), startDate.getMonth(), startDate.getDate() - 7);
-    reRenderTable();
-};
-
-function reRenderTable() {
-    renameHeaders();
-    tds.forEach(item => {
-        if (item.innerHTML) {
-            item.innerHTML = "";
-            item.style.backgroundColor = "rgb(255, 248, 220)";
-        };
-        let arrTh = document.body.getElementsByTagName('th');
-        arrTh = Array.from(arrTh);
-        let thId = arrTh.map(item => item.getAttribute('id'));
-        eventMap.forEach((value, key) => {
-            thId.forEach(item => {
-                if (value.currentDay == item) decorated(key, value);
-            })
-        });
-    });
-};
-
-function renameHeaders() {
-    let date = new Date(startDate);
-    hrs.slice(1).forEach((item, index) => {
-        let days = item.innerHTML.split(" ");
-        item.innerHTML = days[0] + " " + date.getDate();
-        item.setAttribute('id', stringData(date));
-        date.setDate(date.getDate() + 1);
-    });
-};
 
 function createTable() {
-    let date = new Date(startDate);
     let timetable = createTag(container, 'table', 'table');
     arrTime.forEach((item, index) => {
         let tr = createTag(timetable, "tr");
         arrDay.forEach((value, i) => {
             if (index == 0) {
-                let th;
-
                 if (value == "Name") {
-                    th = createTag(tr, 'th', "", value);
+                   createTag(tr, 'th', "", value);
                 } else {
-                    th = createTag(tr, 'th', stringData(date), value + date.getDate());
-                    date.setDate(date.getDate() + 1);
+                    createTag(tr, 'th', "", value);
                 }
-                hrs.push(th);
             } else {
                 if (i == 0) createTag(tr, 'td', "", item);
                 else {
-                    let td = createTag(tr, 'td');
-                    tds.push(td);
+                   createTag(tr, 'td');
                 }
             };
         });
@@ -203,10 +136,6 @@ function createTable() {
     setPosition(container);
 };
 
-function stringData(date) {
-    return "" + date.getFullYear() + " " + date.getMonth() + " " + date.getDate();
-
-}
 
 function setPosition(elem) {
     let centerX = document.documentElement.clientWidth;
